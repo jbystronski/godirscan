42ddcc62abed83bd454137499a36b645c489e5e3
jbystronski <jbstr85@gmail.com>
2023-08-09 13:59:24 +0200

    feat: improve execution of custom commands

--- pkg/task/execute.go
+++ pkg/task/execute.go
@@ -1,13 +1,18 @@
 package task
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+
+	k "github.com/eiannone/keyboard"
+	"github.com/jbystronski/godirscan/pkg/terminal"
+	"github.com/jbystronski/godirscan/pkg/utils"
 )
 
-func ExecuteDefault(path string, errHandle func(error)) {
+func ExecuteDefault(path string) {
 	var cmd *exec.Cmd
 
 	switch runtime.GOOS {
@@ -24,13 +29,31 @@ func ExecuteDefault(path string, errHandle func(error)) {
 			cmd = exec.Command("xdg-open", path)
 		}
 	}
-
+	terminal.ClearScreen()
+	fmt.Println(terminal.CurrentTheme.Accent + "Command execution output (press esc to return): " + terminal.ResetFmt)
+	fmt.Println()
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	err := cmd.Run()
 	if err != nil {
-		errHandle(err)
+		utils.ShowErrAndContinue(err)
+		return
+	}
+
+	for {
+
+		_, key, err := k.GetKey()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if key == k.KeyEsc {
+			terminal.ClearScreen()
+			return
+		}
+
 	}
 }
 
@@ -38,11 +61,30 @@ func ExecCommand(input string) {
 	args := strings.Fields(input)
 
 	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdin = os.Stdin
+	//	cmd.Stdin = os.Stdin
+	terminal.ClearScreen()
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
-		panic(err)
+		utils.ShowErrAndContinue(err)
+		return
+	}
+	fmt.Println(terminal.CurrentTheme.Accent + "Command output (press esc to return): " + terminal.ResetFmt)
+	fmt.Println()
+	for {
+
+		_, key, err := k.GetKey()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if key == k.KeyEsc {
+			terminal.ClearScreen()
+			return
+		}
+
 	}
 }
